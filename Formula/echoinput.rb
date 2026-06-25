48ea2b9fbc0870bcba5667d2f716ed0f56820352
Soumo Sarkar <soumosarkar297@gmail.com>
2026-06-25 12:02:05 +0530

    chore: update Homebrew formula with correct tag URL and SHA256

--- Formula/echoinput.rb
+++ Formula/echoinput.rb
@@ -1,8 +1,8 @@
 class Echoinput < Formula
   desc "Privacy-first keyboard visualization overlay"
   homepage "https://github.com/SuperSection/echoinput"
-  url "https://github.com/SuperSection/echoinput/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256_AFTER_RELEASE"
+  url "https://github.com/SuperSection/echoinput/archive/refs/tags/v0.1.0-alpha.1.tar.gz"
+  sha256 "aec128360539fb34bf78ca8e04b12a34929a27f0745f40012ca079177e351d45"
   license "MIT OR Apache-2.0"
 
   depends_on "rust" => :build
